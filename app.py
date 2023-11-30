a9b4b4eb7e622d1d68b5f8ac300a452243ed1bf4
Ashwath <ashwathguru.s@gmail.com>
2023-11-30 16:05:41 +0530

    template attempt

--- app.py
+++ app.py
@@ -1,48 +1,114 @@
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
+from PIL import Image
 
-def main():
-    st.title("Streamlit Chat App with Images")
 
-    # Load and display 4 images in a 2x2 grid on the landing page
-    image1 = st.image("image.jpg", caption="Image 1", use_column_width=True)
-    image2 = st.image("image.jpg", caption="Image 2", use_column_width=True)
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
-    col1, col2 = st.beta_columns(2)
-    image3 = col1.image("image.jpg", caption="Image 3", use_column_width=True)
-    image4 = col2.image("image.jpg", caption="Image 4", use_column_width=True)
 
-    # Add a clickable chat icon in the bottom right corner
-    st.markdown(
-        """
-        <style>
-            .chat-icon {
-                cursor: pointer;
-                font-size: 24px;
-                color: #3498db;
-                position: fixed;
-                bottom: 20px;
-                right: 20px;
-            }
-        </style>
-        """
-    , unsafe_allow_html=True)
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-    if st.button("💬", key="chat-btn", class_="chat-icon"):
-        # If the button is clicked, show the chat pop-up
-        st.markdown(
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
+img_contact_form = Image.open("images/yt_contact_form.png")
+img_lottie_animation = Image.open("images/yt_lottie_animation.png")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hi, I am Sven :wave:")
+    st.title("A Data Analyst From Germany")
+    st.write(
+        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+    )
+    st.write("[Learn More >](https://pythonandvba.com)")
+
+# ---- WHAT I DO ----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
             """
-            <div id="chat-popup" style="position: fixed; bottom: 0; right: 0; padding: 20px; width: 400px; height: 400px; background: #fff; border-top-left-radius: 10px;">
-                <h2>Chat Popup</h2>
-                <textarea style="width: 100%; height: 200px; margin-bottom: 10px;"></textarea>
-                <button onclick="document.getElementById('chat-popup').style.display='none'">Close</button>
-            </div>
-            <script>
-                document.getElementById("chat-popup").addEventListener("click", function(event) {
-                    event.stopPropagation();
-                });
-            </script>
-            """
-        , unsafe_allow_html=True)
+            On my YouTube channel I am creating tutorials for people who:
+            - are looking for a way to leverage the power of Python in their day-to-day work.
+            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
+            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
+            - are working with Excel and found themselves thinking - "there has to be a better way."
 
-if __name__ == "__main__":
-    main()
+            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            """
+        )
+        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
+
+# ---- PROJECTS ----
+with st.container():
+    st.write("---")
+    st.header("My Projects")
+    st.write("##")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_lottie_animation)
+    with text_column:
+        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
+        st.write(
+            """
+            Learn how to use Lottie Files in Streamlit!
+            Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
+            In this tutorial, I'll show you exactly how to do it
+            """
+        )
+        st.markdown("[Watch Video...](https://youtu.be/TXSOitGoINE)")
+with st.container():
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_contact_form)
+    with text_column:
+        st.subheader("How To Add A Contact Form To Your Streamlit App")
+        st.write(
+            """
+            Want to add a contact form to your Streamlit website?
+            In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
+            """
+        )
+        st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
+
+# ---- CONTACT ----
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
+    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
