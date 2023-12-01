54d959f830afa43cc0091529513966ef8f247429
Ashwath <ashwathguru.s@gmail.com>
2023-12-01 18:55:25 +0530

    UI cleanup

--- app.py
+++ app.py
@@ -18,39 +18,6 @@ import streamlit.components.v1 as components
 # import API key from .env file
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-def streamlit_menu():
-    selected = option_menu(
-        menu_title="IT Service Management",  # required
-        options=["ReportGPT", "WikiGPT"],  # required
-        icons=["book", "envelope"],  # optional
-        menu_icon="cast",  # optional
-        default_index=0,  # optional
-        orientation="horizontal",
-        styles={
-            "container": {"padding": "0!important", "background-color": "#dbdb32"},
-            "icon": {"color": "#32bcdb", "font-size": "25px"},  # Change font color to yellow
-            "nav-link": {
-                "font-size": "25px",
-                "text-align": "left",
-                "margin": "0px",
-                "--hover-color": "#eee",
-            },
-            "nav-link-selected": {"background-color": "green"},
-        },
-    )
-    return selected
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        st.error(f"Failed to load Lottie animation. Status code: {r.status_code}")
-        return None
-    try:
-        return r.json()
-    except ValueError as e:
-        st.error(f"Error parsing Lottie JSON: {e}")
-        return None
-
 # Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
@@ -102,21 +69,6 @@ def transcribe_audio(file_path):
 
     return transcript["text"]
 
-def customMarkdown(msg):
-    placeholder = st.empty()
-    audio_code = f"""
-    <audio id="audioPlayer" autoplay>
-        <source src="{msg}" type="audio/mp3">
-        Your browser does not support the audio element.
-    </audio>
-    <script>
-        document.getElementById("audioPlayer").setAttribute("src", "{msg}");
-        document.getElementById("audioPlayer").play();
-    </script>
-    """
-    st.markdown(audio_code, unsafe_allow_html=True)
-    placeholder.empty()
-
 def text_to_speech(text):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")[:-3]  # Format with milliseconds
     filename = f"output_{timestamp}.mp3"
@@ -128,42 +80,6 @@ def text_to_speech(text):
     # Generate a data URI for the audio
     audio_uri = f"data:audio/mp3;base64,{audio_base64}"
     st.cache_data.clear()
-    # Display the audio player using HTML and JavaScript
-    #audio_code = f"""
-    #<audio autoplay controls>
-    #    <source src="{audio_uri}" type="audio/mp3">
-    #    Your browser does not support the audio element.
-    #</audio>
-    #"""
-
-    # Display the audio player using HTML5 with autoplay and JavaScript
-    #audio_code = f"""
-    #<audio id="audioPlayer" autoplay controls>
-    #    <source src="{audio_uri}" type="audio/mp3">
-    #    Your browser does not support the audio element.
-    #</audio>
-    #<script>
-    #    var audio = document.getElementById("audioPlayer");
-    #    audio.onended = function() {{
-    #        // Reload the audio source to enable autoplay on subsequent calls
-    #        audio.src = "{audio_uri}";
-    #        audio.load();
-    #        audio.play();
-    #    }};
-    #</script>
-    #"""
-
-    #audio_code = f"""
-    ##<audio id="audioPlayer" autoplay controls>
-    ##    <source src="{audio_uri}" type="audio/mp3">
-    ##    Your browser does not support the audio element.
-    ##</audio>
-    ##<script>
-    ##    document.getElementById("audioPlayer").setAttribute("src", "{audio_uri}");
-    ##    document.getElementById("audioPlayer").play();
-    ##</script>
-    #"""
-
     audio_code = f"""
     <audio id="audioPlayer" autoplay>
         <source src="{audio_uri}" type="audio/mp3">
@@ -175,57 +91,10 @@ def text_to_speech(text):
     </script>
     """
     st.markdown(audio_code, unsafe_allow_html=True)
-    #customMarkdown(audio_uri)
 
-def WikiGPT():
-    st.title("WikiGPT")
-    tab1, tab2 = st.tabs(["Speak", "Chat"])
-    # Record Audio tab
-    with tab1:
-        audio_bytes = audio_recorder()
-        st.image("images/report_charts.jpg")
-        st.write("Ask questions about the report")
-        if audio_bytes:
-            st.cache_data.clear()
-            #option to replay audio
-            #st.audio(audio_bytes, format="audio/wav")
-            save_audio_file(audio_bytes, "mp3")
-            audio_file_path = max(
-                [f for f in os.listdir(".") if f.startswith("audio")],
-                key=os.path.getctime,
-            )
-            # Transcribe the audio file
-            transcript_text = transcribe_audio(audio_file_path)
-            # Display the transcript
-            st.header("Transcript")
-            st.write(transcript_text)
-            query=transcript_text
-            response=get_answer_csv(query)
-            if response != "":
-                st.write(response)
-                js_code="""
-                var u = new SpeechSynthesisUtterance();
-                u.text = "{response}";
-                u.lang = 'en-US';
-                speechSynthesis.speak(u);
-                """.format(response=response)
-                my_html = f"<script>{js_code}</script>"
-                components.html(my_html)
-
-    #Chat Tab
-    with tab2:
-        query = st.text_area("Ask any question related to the tickets")
-        button = st.button("Submit")
-        st.image("images/report_charts.jpg")
-        st.write("Ask questions about the report")
-        if button:
-            response=get_answer_csv(query)
-            if response != "":
-                st.write(response)
 def reportsGPT():
-    st.title("ReportGPT")
     st.image("images/report_charts.jpg")
-    st.write("Ask questions about the report.")
+    st.write("Ask about it")
     tab1, tab2 = st.tabs(["Speak", "Chat"])
     # Record Audio tab
     with tab1:
@@ -258,26 +127,17 @@ def reportsGPT():
                 components.html(my_html)
     #Chat Tab
     with tab2:
-        query = st.text_area("Ask any question related to the tickets")
+        query = st.text_area("Ask any question related to the tickets",label_visibility="hidden")
         button = st.button("Submit")
         if button:
             response=get_answer_csv(query)
             if response != "":
                 st.write(response)
 
-# ---- LOAD ASSETS ----
-lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
-img_contact_form = Image.open("images/yt_contact_form.png")
-img_lottie_animation = Image.open("images/yt_lottie_animation.png")
-quickparts_logo = Image.open("images/quickparts_logo.JPG")
 
-selected = streamlit_menu()
+# Set up the working directory
+working_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(working_dir)
+# Run the main function
+reportsGPT()
 
-if selected == "ReportGPT":
-    # Set up the working directory
-    working_dir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(working_dir)
-    # Run the main function
-    reportsGPT()
-if selected == "WikiGPT":
-    WikiGPT()
